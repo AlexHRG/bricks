c088167a0d48aa4f9e23bf7e31354246365a1264
Oleksandr Shchur <oshchur@intropro.com.ua>
2018-09-05 15:50:23 +0300

    added buffering to speed up

--- src/Brick.java
+++ src/Brick.java
@@ -1,17 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Brick {
 
-    static double W(int row, int pos) {
+    static Map<Integer, double[]> map = new HashMap();
 
+    static double W(int row, int pos) {
         if (row == 0) return 0.0;
 
-        if (pos == 0) return (W(row - 1, pos) + 1) / 2;//first brick in line
-        else if (pos == row) return (W(row - 1, pos - 1) + 1) / 2;//last brick in line
+        double result;
+        double[] array = map.get(row);
 
-        return  (W(row - 1, pos - 1) + 1) / 2 + (W(row - 1, pos) + 1) / 2;
+        if (array == null) {
+            array = new double[row+1];
+        }
+
+        if (array[pos] != 0.0) {
+            return array[pos];
+        }
+
+        if (pos == 0) result = (W(row - 1, pos) + 1) / 2;//first brick in line
+        else if (pos == row) result = (W(row - 1, pos - 1) + 1) / 2;//last brick in line
+        else result =  (W(row - 1, pos - 1) + 1) / 2 + (W(row - 1, pos) + 1) / 2;
+
+        array[pos] = result;
+        map.put(row, array);
+
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println("Pressure = " + Brick.W(3, 0));
+        System.out.println("Pressure = " + Brick.W(322, 156));
     }
 }
 
